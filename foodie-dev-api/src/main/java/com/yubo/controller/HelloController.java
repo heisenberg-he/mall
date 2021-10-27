4eb49606cedc58e9545b27ce251c2414839798b7
hwd <1346331079@qq.com>
2021-10-28 00:44:13 +0800

    redis

--- foodie-dev-api/src/main/java/com/yubo/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/yubo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.yubo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("hello")
 public class HelloController {
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @GetMapping("say")
     public String hello(){
         return "Hello bobo";
     }
+
+    @GetMapping("set")
+    public String set(String key,String value){
+        redisTemplate.opsForValue().set(key,value);
+        return "success";
+    }
+
+    @GetMapping("get")
+    public String get(String key){
+        Object o = redisTemplate.opsForValue().get(key);
+        return (String)o;
+    }
+
+    @GetMapping("delete")
+    public String delete(String key){
+        redisTemplate.delete(key);
+        return "ok";
+    }
+
 }
